8a79743f2fc48567cbf838c0c57679cf3d81e417
Bruno César <bcgmeireles@gmail.com>
2023-01-30 20:40:54 -0300

    adicionando exercícios

--- lib/data/dummy_data.dart
+++ lib/data/dummy_data.dart
@@ -1,19 +1,19 @@
 // ignore_for_file: camel_case_types
 
 class DUMMY_DATA {
-  final List<String> type = ['A', 'B', 'C', 'D'];
+  final List<String> type = ['A', 'B', 'C'];
   final List<Map<String, dynamic>> a = [
     {
       'type': 'A',
       'image': 'assets/images/men.jpg',
-      'name': 'Montanha A',
+      'name': 'Montanha Alpinista',
       'rep': 10,
       'series': 4,
     },
     {
       'type': 'A',
       'image': 'assets/images/abdominal.jpg',
-      'name': 'Abdominal A',
+      'name': 'Abdominal Supra',
       'rep': 10,
       'series': 4,
     },
@@ -29,10 +29,27 @@ class DUMMY_DATA {
     },
     {
       'type': 'B',
-      'image': 'assets/images/abdominal.jpg',
-      'name': 'Abdominal B',
-      'rep': 10,
-      'series': 4,
+      'image': 'assets/images/rosca direta.jpg',
+      'name': 'Rosca Direta',
+      'rep': 12,
+      'series': 3,
+      'weight': 10.0
+    },
+    {
+      'type': 'B',
+      'image': 'assets/images/puxada alta.webp',
+      'name': 'Puxada Alta',
+      'rep': 12,
+      'series': 3,
+      'weight': 10.0
+    },
+    {
+      'type': 'B',
+      'image': 'assets/images/remada baixa.webp',
+      'name': 'Remada Baixa',
+      'rep': 12,
+      'series': 3,
+      'weight': 10.0
     },
   ];
   final List<Map<String, dynamic>> c = [
